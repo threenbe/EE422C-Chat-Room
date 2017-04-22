9e1f298c72447b171a50f47af7ca9ac1842e0b2c
Timberlon Gray <graytimberlon@utexas.edu>
2017-04-22 18:56:18 -0500

    stuff

---
+++ assignment7/ClientMain.java
@@ -0,0 +1,17 @@
+package assignment7;
+
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ClientMain extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		GridPane gridPane = new GridPane();
+	}
+}
