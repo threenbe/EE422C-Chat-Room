8b58ca8b0ee9f52fee8c6b3165b2e3858d4bb3f6
Timberlon Gray <graytimberlon@utexas.edu>
2017-04-22 21:31:06 -0500

    Basic socket crap similar to stuff from class

--- assignment7/ClientMain.java
+++ assignment7/ClientMain.java
@@ -1,11 +1,30 @@
 package assignment7;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import javafx.application.Application;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class ClientMain extends Application{
+	private BufferedReader reader;
+	private PrintWriter writer;
+	
 	public static void main(String[] args) {
+		ClientMain client = new ClientMain();
+		// set up JavaFX window
+		try {
+			@SuppressWarnings("resource")
+			Socket socket = new Socket("127.0.0.1", 5000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 		launch(args);
 	}
 
--- assignment7/ServerMain.java
+++ assignment7/ServerMain.java
@@ -1,2 +1,47 @@
 package assignment7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+
+public class ServerMain extends Observable {
+	public static void main(String[] args) {
+		try {
+			ServerMain server = new ServerMain();
+			@SuppressWarnings("resource")
+			ServerSocket serverSocket = new ServerSocket();
+			while (true) {
+				// make a new thread for managing each client connection
+				Socket clientSocket = serverSocket.accept();
+				ClientObserver writer = new ClientObserver(clientSocket.getOutputStream());
+				new Thread(new Runnable(){
+					@Override
+					public void run() {
+						// TODO Add all the shit for handling a client here AFAIK
+						String message;
+						try {
+							BufferedReader reader = new BufferedReader(
+									new InputStreamReader(clientSocket.getInputStream()));
+							message = reader.readLine();
+							while (message != null) {
+								System.out.println("message was read by server");
+								server.setChanged();
+								server.notifyObservers(message);
+								message = reader.readLine();
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}).start();
+				server.addObserver(writer);
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
