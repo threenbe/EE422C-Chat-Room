ff9d3cf974ab8efa09894df8084f2e7ad22121b6
raiyanac <raiyan.chowdhury@utexas.edu>
2017-04-22 22:23:51 -0500

    cleaned up servermain a bit

--- assignment7/ServerMain.java
+++ assignment7/ServerMain.java
@@ -10,38 +10,40 @@ import java.util.Observable;
 public class ServerMain extends Observable {
 	public static void main(String[] args) {
 		try {
-			ServerMain server = new ServerMain();
-			@SuppressWarnings("resource")
-			ServerSocket serverSocket = new ServerSocket();
-			while (true) {
-				// make a new thread for managing each client connection
-				Socket clientSocket = serverSocket.accept();
-				ClientObserver writer = new ClientObserver(clientSocket.getOutputStream());
-				new Thread(new Runnable(){
-					@Override
-					public void run() {
-						// TODO Add all the shit for handling a client here AFAIK
-						String message;
-						try {
-							BufferedReader reader = new BufferedReader(
-									new InputStreamReader(clientSocket.getInputStream()));
-							message = reader.readLine();
-							while (message != null) {
-								System.out.println("message was read by server");
-								server.setChanged();
-								server.notifyObservers(message);
-								message = reader.readLine();
-							}
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}).start();
-				server.addObserver(writer);
-				
-			}
+			new ServerMain().setUpNetworking();	
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource")
+		ServerSocket serverSocket = new ServerSocket(4242);
+		while (true) {
+			// make a new thread for managing each client connection
+			Socket clientSocket = serverSocket.accept();
+			ClientObserver writer = new ClientObserver(clientSocket.getOutputStream());
+			new Thread(new Runnable(){
+				@Override
+				public void run() {
+					// TODO Add all the shit for handling a client here AFAIK
+					String message;
+					try {
+						BufferedReader reader = new BufferedReader(
+								new InputStreamReader(clientSocket.getInputStream()));
+						message = reader.readLine();
+						while (message != null) {
+							System.out.println("message was read by server");
+							setChanged();
+							notifyObservers(message);
+							message = reader.readLine();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}).start();
+			addObserver(writer);
 		}
 	}
 }
