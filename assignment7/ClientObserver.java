b4beef055f49896eabf0a03fc70d3675d5ed13d2
raiyanac <raiyan.chowdhury@utexas.edu>
2017-04-25 20:38:45 -0500

    clientobserver

--- assignment7/ClientObserver.java
+++ assignment7/ClientObserver.java
@@ -9,15 +9,14 @@ public class ClientObserver extends PrintWriter implements Observer {
 	public ClientObserver(OutputStream out) {
 		super(out);
 	}
-	
-	public void update(Observable obs, Message message) {
-		this.println("User " + Integer.toString(message.getUserNum()) + " said: " + message.getMsg());
-		this.flush();
-	}
 
 	@Override
 	public void update(Observable obs, Object obj) {
-		this.println(obj);
-		this.flush();
+		String message = "";
+		if (obj instanceof Message) {
+			message = "User " + Integer.toString(((Message)obj).getUserNum()) + " said: " + ((Message)obj).getMsg();
+			this.println(message);
+			this.flush();
+		}
 	}
 }
